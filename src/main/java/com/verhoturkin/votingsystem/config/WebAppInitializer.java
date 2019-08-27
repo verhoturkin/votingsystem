08305ec179828f76b138931794c29fca5d808968
Verkhoturkin <verhoturkin@gmail.com>
2019-08-27 16:36:28 +0500

    add initial spring web app initializer

---
+++ src/main/java/com/verhoturkin/votingsystem/config/WebAppInitializer.java
@@ -0,0 +1,29 @@
+package com.verhoturkin.votingsystem.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{AppConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+
+    }
+
+//    @Override
+//    public void onStartup(ServletContext servletContext) throws ServletException {
+//        FilterRegistration.Dynamic encodingFilter = servletContext.addFilter("encoding-filter", new CharacterEncodingFilter());
+//        encodingFilter.setInitParameter("encoding", "UTF-8");
+//        encodingFilter.setInitParameter("forceEncoding", "true");
+//        encodingFilter.addMappingForUrlPatterns(null, true, "/*");
+//    }
+}
