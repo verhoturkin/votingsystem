de01a5603670698c4278ff6128e4b613d4a9c8ee
Verkhoturkin <verhoturkin@gmail.com>
2019-08-27 16:31:15 +0500

    add base HasId interface

---
+++ src/main/java/com/verhoturkin/votingsystem/HasId.java
@@ -0,0 +1,20 @@
+package com.verhoturkin.votingsystem;
+
+import org.springframework.util.Assert;
+
+public interface HasId {
+
+    Integer getId();
+
+    void setId(Integer id);
+
+    default boolean isNew() {
+        return getId() == null;
+    }
+
+    // doesn't work for hibernate lazy proxy
+    default int id() {
+        Assert.notNull(getId(), "Entity must has id");
+        return getId();
+    }
+}
