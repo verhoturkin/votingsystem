c5b4d9e2781401ff445f8ca5a78b9a75d4de6171
Verkhoturkin <verhoturkin@gmail.com>
2019-08-27 16:25:17 +0500

    add initial spring database configuration

---
+++ src/main/java/com/verhoturkin/votingsystem/config/DbConfig.java
@@ -0,0 +1,118 @@
+package com.verhoturkin.votingsystem.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.Resource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+import static org.hibernate.cfg.AvailableSettings.*;
+import static org.hibernate.cfg.AvailableSettings.DIALECT;
+
+@Configuration
+@EnableTransactionManagement
+@PropertySource(value = "classpath:db/hsqldb.properties")
+@EnableJpaRepositories("com.verhoturkin.**.repository")
+public class DbConfig {
+
+    @Value("${database.url}")
+    private String dbUrl;
+
+    @Value("${database.username}")
+    private String dbUsername;
+
+    @Value("${database.password}")
+    private String dbPassword;
+
+    @Value("${jpa.showSql}")
+    private String showSql;
+
+    @Value("classpath:db/initDB.sql")
+    private Resource schemaScript;
+
+    @Value("classpath:db/populateDB.sql")
+    private Resource dataScript;
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.hsqldb.jdbcDriver");
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(dbUsername);
+        dataSource.setPassword(dbPassword);
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource());
+        em.setPackagesToScan("io.github.verhoturkin.**.model");
+
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        em.setJpaProperties(additionalProperties());
+        return em;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(emf);
+
+        return transactionManager;
+    }
+
+    //init and populate DB https://stackoverflow.com/questions/16038360/initialize-database-without-xml-configuration-but-using-configuration
+    @Bean
+    public DataSourceInitializer dataSourceInitializer(DataSource dataSource) {
+        DataSourceInitializer initializer = new DataSourceInitializer();
+        initializer.setDataSource(dataSource);
+        initializer.setDatabasePopulator(databasePopulator());
+        return initializer;
+    }
+
+    private DatabasePopulator databasePopulator() {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.setSqlScriptEncoding("UTF-8");
+        populator.addScript(schemaScript);
+        populator.addScript(dataScript);
+        return populator;
+    }
+
+    private Properties additionalProperties() {
+        Properties properties = new Properties();
+        properties.setProperty(SHOW_SQL, showSql);
+        properties.setProperty(FORMAT_SQL, "true");
+        properties.setProperty(USE_SQL_COMMENTS, "true");
+        properties.setProperty(JPA_PROXY_COMPLIANCE, "false");
+//        properties.setProperty(CACHE_REGION_FACTORY, "org.hibernate.cache.jcache.internal.JCacheRegionFactory");
+//        properties.setProperty(PROVIDER, "org.ehcache.jsr107.EhcacheCachingProvider");
+//        properties.setProperty(USE_SECOND_LEVEL_CACHE, "true");
+        properties.setProperty(USE_QUERY_CACHE, "false");
+        properties.setProperty(DIALECT, "org.hibernate.dialect.HSQLDialect");
+
+        return properties;
+    }
+}
