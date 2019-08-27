fe9760d0b0f179d2f2e09bfed497c7bb2345fd9e
Verkhoturkin <verhoturkin@gmail.com>
2019-08-27 23:01:01 +0500

    add findByEmail

--- src/main/java/com/verhoturkin/votingsystem/repository/UserRepository.java
+++ src/main/java/com/verhoturkin/votingsystem/repository/UserRepository.java
@@ -2,13 +2,11 @@ package com.verhoturkin.votingsystem.repository;
 
 import com.verhoturkin.votingsystem.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 @Transactional(readOnly = true)
@@ -16,9 +14,6 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
     List<User> findAllByOrderByNameAscEmailAsc();
 
-    @Transactional
-    @Modifying
-    @Query("DELETE FROM User u WHERE u.id=:id")
-    int delete(@Param("id") int id);
+    Optional<User> findByEmail(String email);
 }
 
