79c646f62ae5c11cf5ee604827ede20455b8ebb2
Verkhoturkin <verhoturkin@gmail.com>
2019-08-27 21:09:58 +0500

    add model mapper

--- src/main/java/com/verhoturkin/votingsystem/config/AppConfig.java
+++ src/main/java/com/verhoturkin/votingsystem/config/AppConfig.java
@@ -1,9 +1,16 @@
 package com.verhoturkin.votingsystem.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan({"com.verhoturkin.**.config", "com.verhoturkin.**.service"})
 public class AppConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
