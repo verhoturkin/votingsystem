ed08577f1929166c2a1dc83066067296a071adb3
Verkhoturkin <verhoturkin@gmail.com>
2019-08-31 20:56:09 +0500

    password - write only

--- src/main/java/com/verhoturkin/votingsystem/to/UserDto.java
+++ src/main/java/com/verhoturkin/votingsystem/to/UserDto.java
@@ -1,14 +1,26 @@
 package com.verhoturkin.votingsystem.to;
 
-public class UserDto extends BaseDto{
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Objects;
+
+public class UserDto extends BaseDto {
 
     private String name;
 
     private String email;
 
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     public UserDto() {
+    }
+
+    public UserDto(Integer id, String name, String email, String password) {
+        super(id);
+        this.name = name;
+        this.email = email;
+        this.password = password;
     }
 
     public UserDto(String name, String email, String password) {
@@ -39,5 +51,22 @@ public class UserDto extends BaseDto{
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserDto userDto = (UserDto) o;
+
+        return Objects.equals(id, userDto.id) &&
+                Objects.equals(name, userDto.name) &&
+                Objects.equals(email, userDto.email) &&
+                Objects.equals(password, userDto.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, email, password);
     }
 }
