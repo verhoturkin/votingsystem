9040ed205b98ddf35a75ee8ef2de7f9971a51958
Verkhoturkin <verhoturkin@gmail.com>
2019-08-31 20:55:00 +0500

    add custom jackson object mapper

--- src/main/java/com/verhoturkin/votingsystem/config/WebConfig.java
+++ src/main/java/com/verhoturkin/votingsystem/config/WebConfig.java
@@ -1,10 +1,15 @@
 package com.verhoturkin.votingsystem.config;
 
+import com.verhoturkin.votingsystem.web.json.JacksonObjectMapper;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -18,5 +23,16 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
     }
 
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(createJackson2HttpMessageConverter());
+    }
+
+    private HttpMessageConverter<Object> createJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(JacksonObjectMapper.getMapper());
+
+        return converter;
+    }
 
 }
