4bbb9feb6a47f46295a59485c85cca2b6cc88080
Verkhoturkin <verhoturkin@gmail.com>
2019-09-01 17:04:38 +0500

    configure modelmapper

--- src/main/java/com/verhoturkin/votingsystem/config/AppConfig.java
+++ src/main/java/com/verhoturkin/votingsystem/config/AppConfig.java
@@ -1,16 +1,25 @@
 package com.verhoturkin.votingsystem.config;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import static org.modelmapper.config.Configuration.AccessLevel.PRIVATE;
+
 @Configuration
-@ComponentScan({"com.verhoturkin.**.config", "com.verhoturkin.**.service"})
+@ComponentScan({"com.verhoturkin.**.config", "com.verhoturkin.**.service", "com.verhoturkin.**.mapper"})
 public class AppConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setFieldMatchingEnabled(true)
+                .setSkipNullEnabled(true)
+                .setFieldAccessLevel(PRIVATE);
+        return mapper;
     }
 }
