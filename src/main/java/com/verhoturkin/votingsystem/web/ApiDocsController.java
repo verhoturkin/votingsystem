ab4cbe89be61840aa645c40e7eda820bfb077bad
Verkhoturkin <verhoturkin@gmail.com>
2019-09-11 18:50:56 +0500

    root redirects to documentation

--- src/main/java/com/verhoturkin/votingsystem/web/ApiDocsController.java
+++ src/main/java/com/verhoturkin/votingsystem/web/ApiDocsController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ApiDocsController {
 
+    @GetMapping(value = "/")
+    public String root() {
+        return "redirect:/api-docs";
+    }
+
     @GetMapping(value = "/api-docs")
     public String getDocs() {
         return "api-docs";
